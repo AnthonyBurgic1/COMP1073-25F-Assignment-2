c7fc6e591377da34ad0ef1f3b132f7f1c72f07b7
Anthony Burgic <144724632+AnthonyBurgic1@users.noreply.github.com>
2025-11-03 10:16:55 -0500

    From line 2 - 17 I have added a Class called Smoothie { inside that i have added a constructor, that includes size, base, fruits, & extras

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // My Smoothie Shop is here to help you create delicious and healthy smoothies! \\ 
+class Smoothie {
+    constructor(size, base, fruits, extras) {
+        this.size = size;
+        this.base = base;
+        this.base = fruits;
+        this.base = extras;
+        this.base = {
+            size: { small: 3, medium: 5, large: 7},
+            base: { milk: 1, "almond milk": 1.5, yogurt: 2},
+            extras: { protein: 2, chia: 1, honey: 0.5},
+            fruit: 1
+        };
+    }
+
+    // Let's Calculate the total price of the smoothies \\ 
+    
+}
